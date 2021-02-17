5323a73e02b89342e0cacb21258add9cb451582c
Wiltrouble <wiltrouble@gmail.com>
2021-02-17 18:29:37 -0400

    Observing page state

--- GettinStarted.js
+++ GettinStarted.js
@@ -8,5 +8,10 @@ fixture `Getting Started`
         .typeText('#developer-name', "Wiltrouble")
         .click("#submit-button")
 
+        const articleHeader = await Selector('.result-content').find('h1');
+
+        let headerText = await articleHeader.innerText;
+        console.log(headerText)
+
     });
 
